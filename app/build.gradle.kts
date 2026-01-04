7550c947660e872949a324d4a55443a30d795486
jinno <klvx01@gmail.com>
2026-01-05 01:44:21 +0900

    build: Upgrade Compose BOM and add lifecycle-runtime-compose
    - Upgrades the Compose BOM from version `2024.05.01` to `2024.06.00`.
    - Adds the `androidx.lifecycle:lifecycle-runtime-compose` dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.activity.compose)
 
     // Compose
